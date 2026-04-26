8817c481aa8e54246cec2c164eecc48f0d8afb36
Aditya Lour <louraditya6584@gmail.com>
2026-04-26 18:49:42 +0530

    feat: connect MongoDB before starting server

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const PORT = 5000;
 
+const connectdb = require("./config/db.js");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Working");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectdb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
