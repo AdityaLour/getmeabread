636c504dad7af0456639f3db7f965277d214fd0a
Aditya Lour <louraditya6584@gmail.com>
2026-04-26 18:36:09 +0530

    feat: add MongoDB connection setup

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
