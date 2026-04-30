76ea49b1b848d52649364f358e7e4d169cf9ab71
Aditya Lour <louraditya6584@gmail.com>
2026-05-01 00:45:48 +0530

    feed page with Api integration

--- frontend/src/pages/feed.jsx
+++ frontend/src/pages/feed.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Feed() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      try {
+        const res = await api.get("/api/notes/feed");
+        setNotes(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchFeed();
+  }, []);
+
   return (
-    <>
-      <h1>Welcome to fedd</h1>
-    </>
+    <div>
+      <h2>Feed</h2>
+      {notes.map((note) => (
+        <div key={note._id}>
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+          <p>By:{note.userId?.username}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
